fd0cc16268b43be096b3c4a565bfaec29773e47e
rmilano24 <rmilano24@gmail.com>
2018-01-15 11:56:32 -0800

    allow svg upload

--- wp-content/themes/insomnia/functions.php
+++ wp-content/themes/insomnia/functions.php
@@ -695,3 +695,9 @@ class Insomnia_My_Walker_Nav_Menu extends Walker_Nav_Menu {
     $output .= "\n$indent<ul class=\"dropdown-menu\">\n";
   }
 }
+
+function cc_mime_types($mimes) {
+  $mimes['svg'] = 'image/svg+xml';
+  return $mimes;
+}
+add_filter('upload_mimes', 'cc_mime_types');
