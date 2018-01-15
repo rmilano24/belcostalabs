139db04c93f422a6fa9ea6f14e75ac2159d11b75
rmilano24 <rmilano24@gmail.com>
2018-01-15 13:58:09 -0800

    update footer

--- wp-content/themes/belcosta/framework/content/footer.php
+++ wp-content/themes/belcosta/framework/content/footer.php
@@ -1,195 +1,22 @@
 <footer>
-	<div class="footer">
-		<?php if(get_theme_mod('insomnia_widget_footer', 'enable') == true)  { ?>
-		<div class="footer-area-cont">
-			<div class="container">
-				<div class="row">
-				  <?php $insomnia_widget_footer_count = get_theme_mod( 'insomnia_widget_footer_count', 'four'); ?>
-				    <?php if ($insomnia_widget_footer_count == 'three'): ?>
-					<div class="col-lg-4 col-md-4 col-sm-6 col-xs-12">
-						<div class="footer-widget">
-							<?php if ( is_active_sidebar( 'footer-1' ) ) : ?>
-								<div class="footer-area">
-									<?php dynamic_sidebar( 'footer-1' ); ?>
-								</div>
-							<?php else: ?>	
-							<div class="footer-area">
-								<h3 class="widget-title"><?php esc_html_e( 'Footer Sidebar 1', 'insomnia' ) ?></h3>
-									<div class="textwidget"><?php esc_html_e( 'Insert your widget on "Footer widget 1" sidebar in Apperrance > Widgets', 'insomnia' ) ?></div>
-								</div>
-							<?php endif; ?>
-						</div>
-					</div>
-					<div class="col-lg-4 col-md-4 col-sm-6 col-xs-12">
-						<div class="footer-widget">
-							<?php if ( is_active_sidebar( 'footer-2' ) ) : ?>
-								<div class="footer-area">
-									<?php dynamic_sidebar( 'footer-2' ); ?>
-								</div>
-							<?php else: ?>	
-							<div class="footer-area">
-								<h3 class="widget-title"><?php esc_html_e( 'Footer Sidebar 2', 'insomnia' ) ?></h3>
-									<div class="textwidget"><?php esc_html_e( 'Insert your widget on "Footer widget 2" sidebar in Apperrance > Widgets', 'insomnia' ) ?></div>
-								</div>
-							<?php endif; ?>
-						</div>
-					</div>
-					<div class="col-lg-4 col-md-4 col-sm-6 col-xs-12">
-						<div class="footer-widget">
-							<?php if ( is_active_sidebar( 'footer-3' ) ) : ?>
-								<div class="footer-area">
-									<?php dynamic_sidebar( 'footer-3' ); ?>
-								</div>
-							<?php else: ?>	
-							<div class="footer-area">
-								<h3 class="widget-title"><?php esc_html_e( 'Footer Sidebar 3', 'insomnia' ) ?></h3>
-									<div class="textwidget"><?php esc_html_e( 'Insert your widget on "Footer widget 3" sidebar in Apperrance > Widgets', 'insomnia' ) ?></div>
-								</div>
-							<?php endif; ?>
-						</div>
-					</div>
-     				<?php elseif ($insomnia_widget_footer_count == 'four'): ?>
-					<div class="col-lg-3 col-md-3 col-sm-6 col-xs-12">
-						<div class="footer-widget">
-							<?php if ( is_active_sidebar( 'footer-1' ) ) : ?>
-								<div class="footer-area">
-									<?php dynamic_sidebar( 'footer-1' ); ?>
-								</div>
-							<?php else: ?>	
-							<div class="footer-area">
-								<h3 class="widget-title"><?php esc_html_e( 'Footer Sidebar 1', 'insomnia' ) ?></h3>
-									<div class="textwidget"><?php esc_html_e( 'Insert your widget on "Footer widget 1" sidebar in Apperrance > Widgets', 'insomnia' ) ?></div>
-								</div>
-							<?php endif; ?>
-						</div>
-					</div>
-					<div class="col-lg-3 col-md-3 col-sm-6 col-xs-12">
-						<div class="footer-widget">
-							<?php if ( is_active_sidebar( 'footer-2' ) ) : ?>
-								<div class="footer-area">
-									<?php dynamic_sidebar( 'footer-2' ); ?>
-								</div>
-							<?php else: ?>	
-							<div class="footer-area">
-								<h3 class="widget-title"><?php esc_html_e( 'Footer Sidebar 2', 'insomnia' ) ?></h3>
-									<div class="textwidget"><?php esc_html_e( 'Insert your widget on "Footer widget 2" sidebar in Apperrance > Widgets', 'insomnia' ) ?></div>
-								</div>
-							<?php endif; ?>
-						</div>
-					</div>
-					<div class="col-lg-3 col-md-3 col-sm-6 col-xs-12">
-						<div class="footer-widget">
-							<?php if ( is_active_sidebar( 'footer-3' ) ) : ?>
-								<div class="footer-area">
-									<?php dynamic_sidebar( 'footer-3' ); ?>
-								</div>
-							<?php else: ?>	
-							<div class="footer-area">
-								<h3 class="widget-title"><?php esc_html_e( 'Footer Sidebar 3', 'insomnia' ) ?></h3>
-									<div class="textwidget"><?php esc_html_e( 'Insert your widget on "Footer widget 3" sidebar in Apperrance > Widgets', 'insomnia' ) ?></div>
-								</div>
-							<?php endif; ?>
-						</div>
-					</div>
-					<div class="col-lg-3 col-md-3 col-sm-6 col-xs-12">
-						<div class="footer-widget">
-							<?php if ( is_active_sidebar( 'footer-4' ) ) : ?>
-								<div class="footer-area">
-									<?php dynamic_sidebar( 'footer-4' ); ?>
-								</div>
-							<?php else: ?>	
-							<div class="footer-area">
-								<h3 class="widget-title"><?php esc_html_e( 'Footer Sidebar 4', 'insomnia' ) ?></h3>
-									<div class="textwidget"><?php esc_html_e( 'Insert your widget on "Footer widget 4" sidebar in Apperrance > Widgets', 'insomnia' ) ?></div>
-								</div>
-							<?php endif; ?>
-						</div>
-					</div>
-     				<?php elseif ($insomnia_widget_footer_count == 'five'): ?>
-					<div class="col-lg-3 col-md-3 col-sm-3 col-xs-12">
-						<div class="footer-widget">
-							<?php if ( is_active_sidebar( 'footer-1' ) ) : ?>
-								<div class="footer-area">
-									<?php dynamic_sidebar( 'footer-1' ); ?>
-								</div>
-							<?php else: ?>	
-							<div class="footer-area">
-								<h3 class="widget-title"><?php esc_html_e( 'Footer Sidebar 1', 'insomnia' ) ?></h3>
-									<div class="textwidget"><?php esc_html_e( 'Insert your widget on "Footer widget 1" sidebar in Apperrance > Widgets', 'insomnia' ) ?></div>
-								</div>
-							<?php endif; ?>
-						</div>
-					</div>
-					<div class="col-lg-6 col-md-6 col-sm-6 col-xs-12">
-						<div class="footer-widget">
-							<?php if ( is_active_sidebar( 'footer-2' ) ) : ?>
-								<div class="footer-area">
-									<?php dynamic_sidebar( 'footer-2' ); ?>
-								</div>
-							<?php else: ?>	
-							<div class="footer-area">
-								<h3 class="widget-title"><?php esc_html_e( 'Footer Sidebar 2', 'insomnia' ) ?></h3>
-									<div class="textwidget"><?php esc_html_e( 'Insert your widget on "Footer widget 2" sidebar in Apperrance > Widgets', 'insomnia' ) ?></div>
-								</div>
-							<?php endif; ?>
-						</div>
-					</div>
-					<div class="col-lg-3 col-md-3 col-sm-6 col-xs-12">
-						<div class="footer-widget">
-							<?php if ( is_active_sidebar( 'footer-3' ) ) : ?>
-								<div class="footer-area">
-									<?php dynamic_sidebar( 'footer-3' ); ?>
-								</div>
-							<?php else: ?>	
-							<div class="footer-area">
-								<h3 class="widget-title"><?php esc_html_e( 'Footer Sidebar 3', 'insomnia' ) ?></h3>
-									<div class="textwidget"><?php esc_html_e( 'Insert your widget on "Footer widget 3" sidebar in Apperrance > Widgets', 'insomnia' ) ?></div>
-								</div>
-							<?php endif; ?>
-						</div>
-					</div>
-     				<?php endif; ?> 
+	<div class="footer">	
+		<div class="container">
+			<div class="row">
+				<div class="col-md-6">
+					<a href="http://localhost:8888/belcosta/wordpress/"><img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/images/footer-logo.svg" alt="BelCosta Labs"></a>
+				</div>
+				<div class="col-md-6" id="footer-right">
+				<span class="address">1131 E. South Street  Long Beach, CA 90805   //   310 924 1986</span>
+				<ul class="social">
+					<li><a target="_blank" href="https://www.facebook.com/belcosta.labs.3">Facebook</a></li>
+					<li><a target="_blank" href="https://www.instagram.com/belcostalabs/">Instagram</a></li>
+					<li><a target="_blank" href="mailto:info@belcostalabs.com">Email</a></li>
+				</ul>
 				</div>
 			</div>
+			<hr>
+			<hr>
 		</div>
-		<?php }; ?> 
-		<?php if(get_theme_mod('insomnia_author_footer', 'enable') == true)  { ?>
-		<?php if(get_theme_mod('insomnia_footer_light','enable') == true)  { ?>
-		<div class="footer-copyright">
-		<?php }else{ ?>
-		<div class="footer-copyright light">
-		<?php }; ?>	
-			<div class="container">
-				<div class="row">
-					<div class="col-lg-6 col-md-6 col-sm-6 col-xs-12 not-single">
-						<div class="copyright-info">
-							<?php echo get_theme_mod( 'insomnia_footer_copy', esc_html__( 'Copyright 2016. All Rights Reserved.', 'insomnia' ) ); ?>
-						</div>
-					</div>
-					<div class="col-lg-6 col-md-6 col-sm-6 col-xs-12 pull-right">
-						<div class="author-info">
-							<?php echo get_theme_mod( 'insomnia_footer_author', __( 'Powered by <a href="https://themeforest.net/user/dankov" target="_blank">Dankov</a>', 'insomnia' ) ); ?>
-						</div>
-					</div>
-				</div>
-			</div>
-		</div>
-		<?php }else{ ?>
-		<?php if(get_theme_mod('insomnia_footer_light','enable') == true)  { ?>
-		<div class="footer-copyright">
-		<?php }else{ ?>
-		<div class="footer-copyright light">
-		<?php }; ?>	
-			<div class="container">
-				<div class="row">
-					<div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
-						<div class="copyright-info">
-							<?php echo get_theme_mod( 'insomnia_footer_copy', esc_html__( 'Copyright 2016. All Rights Reserved.', 'insomnia' ) ); ?>
-						</div>
-					</div>
-				</div>
-			</div>
-		</div>
-		<?php }; ?>		
+				 
 	</div>
 </footer>
