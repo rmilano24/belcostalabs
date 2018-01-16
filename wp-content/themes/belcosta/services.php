7420d784759c66a4855dc3aeb9088a7544f53ef3
rmilano24 <rmilano24@gmail.com>
2018-01-16 13:47:26 -0800

    add service template and crete subnav

--- wp-content/themes/belcosta/services.php
+++ wp-content/themes/belcosta/services.php
@@ -3,6 +3,20 @@
 ?>
 <?php get_header();?>
 
+<div class="secondary-nav">
+    <div class="container">
+        <div class="row">
+            <ul>
+                <li><a href="http://">link</a></li>
+                <li><a href="http://">link</a></li>
+                <li><a href="http://">link</a></li>
+                <li><a href="http://">link</a></li>
+                <li><a href="http://">link</a></li>
+            </ul>
+        </div>
+    </div>
+</div>
+
 <div class="default_page">
     <?php  if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
         <div class="container">
